211411e5183974d7f62fa9e611c6514997b725b3
Laurence Stone <larry.stone@gmail.com>
2023-05-03 16:38:48 -0500

    Initial Commit

---
+++ MoneyingWithRepos/MoneyingWithReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyingWithReposApp.swift
+//  MoneyingWithRepos
+//
+//  Created by Laurence Stone on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyingWithReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
