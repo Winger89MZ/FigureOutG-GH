64b4b45e2d4f8eda88c0727c91926fb2b12d72b0
Laurence Stone <larry.stone@gmail.com>
2023-05-05 07:44:10 -0500

    Monkey with Tab Views (CwC project 4)

--- MoneyingWithRepos/ContentView.swift
+++ MoneyingWithRepos/ContentView.swift
@@ -6,18 +6,49 @@
 //
 
 import SwiftUI
-//LET'S SEE IF THIS CHANGE IS SAVED
-//EVEN THOUGH I DID NOT COMMIT
 
 struct ContentView: View {
+    @State var tabIndex = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $tabIndex) {
+            Text("This tab's tag is \(tabIndex)")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "tortoise")
+                        Text("Tab 1")
+                    }
+                }
+                .tag(1)  //tabIndex=0 means this one
+            
+            Button("Take me to Tab #3!") {
+                print("cmon man")
+                tabIndex = 3
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "arrowshape.right")
+                    Text("Tab 2")
+                }
+                .tag(2)
+            }
+            
+            List {
+                ForEach(0..<100) { _ in
+                    Text("This is tab 3!")
+                }
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "hands.clap")
+                    Text("Tab 3")
+                }
+            }
+            .tag(3)
+            
+
         }
-        .padding()
     }
 }
 
