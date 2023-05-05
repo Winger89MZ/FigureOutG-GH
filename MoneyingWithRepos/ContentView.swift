ee3a2920cbdb4a222a296d1dc9c666e7039b6ad4
Laurence Stone <larry.stone@gmail.com>
2023-05-05 09:22:48 -0500

    Monkey with Geometry Reader (CwC 4).

--- MoneyingWithRepos/ContentView.swift
+++ MoneyingWithRepos/ContentView.swift
@@ -11,44 +11,31 @@ struct ContentView: View {
     @State var tabIndex = 1
     
     var body: some View {
-        TabView(selection: $tabIndex) {
-            Text("This tab's tag is \(tabIndex)")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image(systemName: "tortoise")
-                        Text("Tab 1")
+        VStack(spacing: 0) {
+                GeometryReader { g in
+                    Rectangle()
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .foregroundColor(.blue)
+                        .onTapGesture {
+                            print("global x: \(g.frame(in: .global).minX), \(g.frame(in: .global).midX),  \(g.frame(in: .global).maxX)")
+                            print("global y: \(g.frame(in: .global).minY), \(g.frame(in: .global).midY),  \(g.frame(in: .global).maxY)")
+                            print("local x: \(g.frame(in: .local).minX), \(g.frame(in: .local).midX),  \(g.frame(in: .global).maxX)")
+                            print("local y: \(g.frame(in: .local).minY), \(g.frame(in: .local).midY),  \(g.frame(in: .global).maxY)")
+                        }.position(x: 200, y: 200)
+                }
+                    GeometryReader { g in
+                        Rectangle()
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .foregroundColor(.green)
+                            .onTapGesture {
+                                print("global x: \(g.frame(in: .global).minX), \(g.frame(in: .global).midX),  \(g.frame(in: .global).maxX)")
+                                print("global y: \(g.frame(in: .global).minY), \(g.frame(in: .global).midY),  \(g.frame(in: .global).maxY)")
+                                print("local x: \(g.frame(in: .local).minX), \(g.frame(in: .local).midX),  \(g.frame(in: .local).maxX)")
+                                print("local y: \(g.frame(in: .local).minY), \(g.frame(in: .local).midY),  \(g.frame(in: .local).maxY)")
+                            }
+                            .position(x: 200, y: 200)
                     }
                 }
-                .tag(1)  //tabIndex=0 means this one
-            
-            Button("Take me to Tab #3!") {
-                print("cmon man")
-                tabIndex = 3
-            }
-            .tabItem {
-                VStack {
-                    Image(systemName: "arrowshape.right")
-                    Text("Tab 2")
-                }
-                .tag(2)
-            }
-            
-            List {
-                ForEach(0..<100) { _ in
-                    Text("This is tab 3!")
-                }
-            }
-            .tabItem {
-                VStack {
-                    Image(systemName: "hands.clap")
-                    Text("Tab 3")
-                }
-            }
-            .tag(3)
-            
-
-        }
     }
 }
 
