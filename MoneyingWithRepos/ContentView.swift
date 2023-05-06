e3fc46b4ce4990be66badb16570608ee33918817
Laurence Stone <larry.stone@gmail.com>
2023-05-06 12:33:44 -0500

    vertically scrolling rectangles that grow in height; color gradien.

--- MoneyingWithRepos/ContentView.swift
+++ MoneyingWithRepos/ContentView.swift
@@ -8,36 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var randomColor = [
-        Color.blue, Color.red, Color.green, Color.yellow,
-        Color.orange, Color.cyan, Color.pink, Color.mint]
     
     var body: some View {
-        GeometryReader { g in
-            
-            TabView {
-                ForEach(0..<50) { j in
-                    let randIndex = Int.random(in: 0...7)
-                    
-                    Rectangle()
-                        .fill(randomColor[randIndex])
-                        .frame(width: g.size.width - 40,
-                               height: g.size.height - 100)
-                        .cornerRadius(20)
-                        .shadow(color: .gray, radius: 10, x: -5, y: 5)
-                        .overlay(
-                            Image(systemName: "\(j+1).circle"))
-                        .font(.system(size: 250))
-                    
-                }
-                
+        ScrollView {
+            ForEach(0...40, id: \.self) { j in
+                Rectangle()
+                    .fill(RadialGradient(
+                        gradient: Gradient(colors: [.orange, .red]),
+                        center: UnitPoint(x: 0.25, y: 0.75),
+                        startRadius: 0,
+                        endRadius: 200
+                    ))
+                    .frame(height: 100 * pow(1.1, Double(j)))
+                    .cornerRadius(10)
+
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            
         }
 
     }
+
 }
 
 
